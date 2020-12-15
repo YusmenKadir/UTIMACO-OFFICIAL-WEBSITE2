b7e3eab476dcc3fc18a08d40fa47593588713c64
YusmenKadir <67394851+YusmenKadir@users.noreply.github.com>
2020-12-15 18:05:46 +0100

    first commit

---
+++ dist/app.js
@@ -0,0 +1,18 @@
+const menuBtn = document.querySelector(".menu-btn");
+const menuItems = document.querySelector(".menu-items");
+const expandBtn = document.querySelectorAll(".expand-btn");
+
+// humburger toggle
+menuBtn.addEventListener("click", () => {
+  menuBtn.classList.toggle("open");
+  menuItems.classList.toggle("open");
+  console.log("clicked");
+});
+
+// mobile menu expand
+expandBtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    btn.classList.toggle("open");
+    console.log("clicked");
+  });
+});
